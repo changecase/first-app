b1bfda7f5c0f587bf742db59cf5548b5a0e69131
changecase <jeff.ja@gmail.com>
2013-09-11 10:13:57 -0700

    move all the capfile stuf to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,12 +2,30 @@ set :application, "first-app.changecase.net"
 set :repository, "git@github.com:changecase/first-app.git"
 set :domain, 'rigel.dreamhost.com'                                # dreamhost servername where account is located
 
+set :user, 'changecase'                                           # dreamhost account's username
+set :project, '#{application}'                                    # application as its called in the repository
+set :scm, :git
+set :applicationdir, '/home/#{user}/#{application}'               # the standard dreamhost setup
+
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 role :app, domain                                                 # This may be the same as your `Web` server
 role :web, domain                                                 # Your HTTP server, Apache/etc
 role :db,  domain, :primary => true                               # This is where Rails migrations will run
+
+set :deploy_to, applicationdir 
+
+set :deploy_via, :remote_cache                         # deploy config (also the deploy_to above)
+set :git_enable_submodules, 1                          # if you have vendored rails
+set :branch, 'master'
+set :git_shallow_clone, 1
+set :scm_verbose, true
+
+default_run_options[:pty] = true                       # forgo errors when deploying from windows
+ssh_options[:keys] = %w(/home/changecase/.ssh/id_rsa)  # if you are using ssh_keys... and I am!
+set :chmod755, "app config db lib public vendor script script/* public/disp*"
+set :use_sudo, false
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
