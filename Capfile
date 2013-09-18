f44370315bccc956d06ad989183a9be13e6da66f
changecase <jeff.ja@gmail.com>
2013-09-17 19:37:58 -0700

    update to get ssh keys working

--- Capfile
+++ Capfile
@@ -5,6 +5,6 @@ load 'config/deploy' # remove this line to skip loading any of the default tasks
 
 # everything below this was added according to https://github.com/capistrano/capistrano/wiki/2.x-From-The-Beginning
 # modified by dreamhost setup
-
+set :ssh_options, { :forward_agent => true }
 
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :scm_verbose, true
 set :keep_releases, 5
 
 default_run_options[:pty] = true                       # forgo errors when deploying from windows
-ssh_options[:keys] = %w(/home/changecase/.ssh/id_rsa)  # if you are using ssh_keys... and I am!
+ssh_options[:keys] = %w(/home/changecase/.ssh/id_rsa.pb)  # if you are using ssh_keys... and I am!
 set :chmod755, "app config db lib public vendor script script/* public/disp*"
 set :use_sudo, false
 
